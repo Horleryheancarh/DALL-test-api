bf0399806f26d2367bd883d05462cdc1d484afd7
Horleryheancarh <oladipoolayinka7@gmail.com>
2023-08-12 14:50:00 +0100

    added post routes and cloudinary upload

---
+++ database/models/post.js
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+
+const Post = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  prompt: {
+    type: String,
+    required: true
+  },
+  photo: {
+    type: String,
+    required: true
+  },
+})
+
+const PostSchema = model('Post', post);
+
+export default PostSchema;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 
 import connect from './database/connect.js';
 import dalleRoutes from './routes/dalle.routes.js'
+import postRoutes from './routes/post.routes.js'
 
 dotenv.config();
 
@@ -12,9 +13,10 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }))
 
 app.use('/api/v1/dalle', dalleRoutes);
+app.use('/api/v1/post', postRoutes);
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from DALL.E' })
+  res.status(200).json({ message: 'Hello from DALL-E!' })
 })
 
 const startServer = async() => {
